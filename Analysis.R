367cc4c9a6ca586fba61d8398c08959d1bd7dafc
“zoehorlick” <zoehorlick@icloud.com>
2025-09-10 15:55:53 -0700

    Initial commit

---
+++ Analysis.R
@@ -0,0 +1,9 @@
+# view the first few rows of mtcars dataset
+head(mtcars)
+
+# calculate the average mpg among all cars, save it as a variable
+mean_mpg <- mean(mtcars$mpg)
+
+# print the mean mpg 
+mean_mpg
+
